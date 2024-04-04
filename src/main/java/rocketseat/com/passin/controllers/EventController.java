7fbea92bc9206cf41674e9348b3421ae1967ab7d
Camila <alvescamila87@gmail.com>
2024-04-04 16:54:23 -0300

    create endpoint get to attendee event

--- src/main/java/rocketseat/com/passin/controllers/EventController.java
+++ src/main/java/rocketseat/com/passin/controllers/EventController.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+import rocketseat.com.passin.dto.attendee.AttendesListResponseDTO;
 import rocketseat.com.passin.dto.event.EventIdDTO;
 import rocketseat.com.passin.dto.event.EventRequestDTO;
 import rocketseat.com.passin.dto.event.EventResponseDTO;
+import rocketseat.com.passin.services.AttendeeService;
 import rocketseat.com.passin.services.EventService;
 
 @RestController
@@ -14,20 +16,27 @@ import rocketseat.com.passin.services.EventService;
 @RequiredArgsConstructor
 public class EventController {
 
-    private final EventService service;
+    private final EventService eventService;
+    private final AttendeeService attendeeService;
 
     @GetMapping("/{eventId}")
     public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String eventId){
-        EventResponseDTO event = this.service.getEventDetail(eventId);
+        EventResponseDTO event = this.eventService.getEventDetail(eventId);
         return ResponseEntity.ok(event);
     }
 
     @PostMapping
     public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder){
-        EventIdDTO eventIdDTO = this.service.createEvent(body);
+        EventIdDTO eventIdDTO = this.eventService.createEvent(body);
 
         var uri = uriComponentsBuilder.path("/events/{eventId}").buildAndExpand(eventIdDTO.eventId()).toUri();
 
         return ResponseEntity.created(uri).body(eventIdDTO);
+    }
+
+    @GetMapping("/attendees/{eventId}")
+    public ResponseEntity<AttendesListResponseDTO> getEventAttendees(@PathVariable String eventId){
+        AttendesListResponseDTO attendesListResponse = this.attendeeService.getEventsAttendee(eventId);
+        return ResponseEntity.ok(attendesListResponse);
     }
 }
--- src/main/java/rocketseat/com/passin/services/AttendeeService.java
+++ src/main/java/rocketseat/com/passin/services/AttendeeService.java
@@ -16,8 +16,8 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class AttendeeService {
-    private AttendeeRepository attendeeRepository;
-    private CheckInRepository checkinRepository;
+    private final AttendeeRepository attendeeRepository;
+    private final CheckInRepository checkinRepository;
 
     public List<Attendee> getAllAttendeesFromEvent(String eventId){
         return this.attendeeRepository.findByEventId(eventId);
